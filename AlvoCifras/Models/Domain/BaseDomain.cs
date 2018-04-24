f59e521f87d1b3547613b5a03f9fba569fe77db1
Gabriel Louback <bieldozelda2@hotmail.com>
2018-04-24 17:52:19 -0300

    Initial

---
+++ AlvoCifras/Models/Domain/BaseDomain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlvoCifras.Models.Domain
+{
+    public class BaseDomain
+    {
+        public int Id { get; set; }
+    }
+}
